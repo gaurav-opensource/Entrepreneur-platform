1be36092f6d6c220c9cae128e60e3336b6e26a38
gaurav-opensource <gauravopensource95@gmail.com>
2025-04-28 14:22:13 +0530

    packege.json

--- frontend/app/utils/api.ts
+++ frontend/app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://entrepreneur-platform-ot9w.vercel.app/',
 });
 
 export default api;
