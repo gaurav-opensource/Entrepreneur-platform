576246c7e468590e75cc49f47433aeb7818df16b
gaurav-opensource <gauravopensource95@gmail.com>
2025-04-28 10:44:01 +0530

    Initial project setup

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 bg-gray-800 text-white">
+      <ul className="flex space-x-4">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/resources">Resources</Link></li>
+        <li><Link href="/pricing">Pricing</Link></li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
